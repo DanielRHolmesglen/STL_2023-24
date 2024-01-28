6e5e148b647e3bc72a4547c42dcff031c3710989
SamB <125520361+SamBou2501@users.noreply.github.com>
2024-01-29 05:23:35 +1100

    Adding in initial audio and shader managers into scene

---
+++ Assets/Sam/Scripts/AudioManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource backgroundAmbience;
+    public AudioSource shipHum;
+    public AudioSource doorOpen;
+    //public AudioSource footsteps;
+    //public AudioSource randomChatter;
+    public AudioSource airCon;
+
+    //public float footstepsInterval = 12f;
+    public float doorOpenMinInterval = 15f;
+    public float doorOpenMaxInterval = 30f;
+    public float airConInterval = 35f;
+
+    public float noiseReductionDelay = 20f;
+    public float decreasePercentage = 0.50f; // The total percentage by which to decrease the volume
+    public float decreaseDuration = 20f;  // The duration over which to decrease the volume
+
+
+    void Start()
+    {
+        ///start playing background looping sounds 
+        backgroundAmbience.loop = true;
+        backgroundAmbience.Play();
+
+        shipHum.loop = true;
+        shipHum.Play();
+
+        ///start corroutines
+        StartCoroutine(PlayDoorOpening());
+        //StartCoroutine(PlayFootsteps());
+        StartCoroutine(PlayAirCon());
+
+
+        StartCoroutine(manageAudioVolumes());
+
+
+
+
+
+
+    }
+
+    void Update()
+    {
+        /// Adjust the volume of sounds here (e.g., using Mathf.Lerp)
+        
+        //randomChatter.volume = Mathf.Lerp(0.2f, 0.8f, Mathf.PingPong(Time.time, 1f));
+        shipHum.volume = Mathf.Lerp(0.2f, 0.8f, Mathf.PingPong(Time.time, 1f));
+
+    }
+
+    IEnumerator PlayDoorOpening()
+    {
+        while (true)
+        {
+            float interval = Random.Range(doorOpenMinInterval, doorOpenMaxInterval);
+
+            yield return new WaitForSeconds(interval);
+
+            doorOpen.Play();
+        }
+    }
+
+    /*
+    IEnumerator PlayFootsteps()
+    {
+        while (true)
+        {
+            nextFootstepsTime = footstepsInterval;
+
+            yield return new WaitForSeconds(footstepsInterval);
+            footsteps.Play();
+        }
+    }
+    */
+
+    IEnumerator PlayAirCon()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(airConInterval);
+            airCon.Play();
+        }
+    }
+
+    IEnumerator DecreaseVolumeOverTime(AudioSource audioSource)
+    {
+        float startVolume = audioSource.volume;
+        float targetVolume = startVolume * (1.0f - decreasePercentage); // Calculate the target volume
+
+        float startTime = Time.time;
+
+        while (Time.time - startTime < decreaseDuration)
+        {
+            float t = (Time.time - startTime) / decreaseDuration;
+            audioSource.volume = Mathf.Lerp(startVolume, targetVolume, t);
+            yield return null;
+        }
+
+        audioSource.volume = targetVolume;
+    }
+
+    private IEnumerator manageAudioVolumes()
+    {
+        yield return new WaitForSeconds(noiseReductionDelay);
+
+        StartCoroutine(DecreaseVolumeOverTime(backgroundAmbience));
+        StartCoroutine(DecreaseVolumeOverTime(shipHum));
+        StartCoroutine(DecreaseVolumeOverTime(doorOpen));
+        StartCoroutine(DecreaseVolumeOverTime(airCon));
+
+
+    }
+}
---
+++ Assets/Sam/Scripts/ShaderController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderController : MonoBehaviour
+{
+
+    public Material materialToControl;
+    ///[Range(0.0f, 1.0f)] public float dimness = 0.5f;
+
+    public Color targetColor = Color.yellow; // Specify the target color in the Inspector
+    public float colorChangeDuration = 10.0f; // Set the duration in the Inspector
+    public Color dimColor = Color.black;
+    public float dimColorDuration = 10.0f; // Set the duration in the Inspector
+
+    public float speed = 1.0f;
+    public float targetSpeed = 2.0f;
+    public float speedChangeDuration = 3.0f;
+
+    ///public float targetDimness = 0.3f;
+    ///public float dimnessChangeDuration = 2.0f;
+
+    private float time = 0.0f;
+
+
+    public void Start()
+    {
+        StartCoroutine(ChangeColorOverTime(targetColor, colorChangeDuration));
+
+        StartCoroutine(ChangeSpeedOverTime(targetSpeed, speedChangeDuration));
+
+        ///StartCoroutine(ChangeDimnessOverTime(targetDimness, dimnessChangeDuration));
+
+
+    }
+
+    void Update()
+    {
+        // Update the time based on speed
+        ///time += Time.deltaTime * timeSpeed;  
+        ///WE COULD USE THIS FOR A "how long do u want experience" option?
+
+        // Set shader properties
+        ///materialToControl.SetFloat("_Dimness", dimness);
+        materialToControl.SetFloat("_Time", time);
+    }
+
+    IEnumerator ChangeColorOverTime(Color targetColor, float duration)
+    {
+        float startTime = Time.time;
+        Color startColor = materialToControl.GetColor("_Color");
+
+        while (Time.time - startTime < duration)
+        {
+            float t = (Time.time - startTime) / duration;
+            Color lerpedColor = Color.Lerp(startColor, targetColor, t);
+            materialToControl.SetColor("_Color", lerpedColor);
+            yield return null;
+        }
+
+        materialToControl.SetColor("_Color", targetColor);
+
+        //start this coroutine again one after finishing this coroutine, with different values. It will keep looping though currently.
+        StartCoroutine(ChangeColorOverTime(dimColor, dimColorDuration));
+        
+    }
+
+    IEnumerator ChangeSpeedOverTime(float targetSpeed, float duration)
+    {
+        float startTime = Time.time;
+        float startSpeed = materialToControl.GetFloat("_Speed");
+
+        while (Time.time - startTime < duration)
+        {
+            float t = (Time.time - startTime) / duration;
+            float lerpedSpeed = Mathf.Lerp(startSpeed, targetSpeed, t);
+            materialToControl.SetFloat("_speed", lerpedSpeed);
+            yield return null;
+        }
+
+        materialToControl.SetFloat("_speed", targetSpeed);
+    }
+
+    /*
+    IEnumerator ChangeDimnessOverTime(float targetDimness, float duration)
+    {
+        float startTime = Time.time;
+        float startDimness = dimness;
+
+        while (Time.time - startTime < duration)
+        {
+            float t = (Time.time - startTime) / duration;
+            dimness = Mathf.Lerp(startDimness, targetDimness, t);
+            yield return null;
+        }
+
+        dimness = targetDimness;
+    }
+    */
+}
